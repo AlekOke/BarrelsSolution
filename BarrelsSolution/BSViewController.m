b007d7a6a01a3e1dd9a4941c8bef8ff097718854
Aleksey Prukhodko <prukhodko@dstudiosoft.com>
2015-12-20 21:13:51 +0200

    Updated UI.

--- BarrelsSolution/BSViewController.m
+++ BarrelsSolution/BSViewController.m
@@ -19,20 +19,8 @@
 @property (nonatomic, strong) NSArray<BSNode *> *solution;
 @property (nonatomic, assign) NSUInteger currentNodeIndex;
 
-// container 1
-@property (nonatomic, weak) IBOutlet UILabel *containerNameLabel1;
-@property (nonatomic, weak) IBOutlet UILabel *containerCapacityLabel1;
-@property (nonatomic, weak) IBOutlet UILabel *containerValueLabel1;
-
-// container 2
-@property (nonatomic, weak) IBOutlet UILabel *containerNameLabel2;
-@property (nonatomic, weak) IBOutlet UILabel *containerCapacityLabel2;
-@property (nonatomic, weak) IBOutlet UILabel *containerValueLabel2;
-
-// container 3
-@property (nonatomic, weak) IBOutlet UILabel *containerNameLabel3;
-@property (nonatomic, weak) IBOutlet UILabel *containerCapacityLabel3;
-@property (nonatomic, weak) IBOutlet UILabel *containerValueLabel3;
+@property (nonatomic, strong) IBOutletCollection(UILabel) NSArray<UILabel *> *containerValuesLabels;
+@property (nonatomic, strong) IBOutletCollection(UILabel) NSArray<UILabel *> *containerCapacityLabels;
 
 // controls
 @property (nonatomic, weak) IBOutlet UILabel *pageLabel;
@@ -48,28 +36,18 @@
 	self.currentNodeIndex = nodeIndex;
 	BSNode *node = self.solution[nodeIndex];
 	
-	// update controls
 	self.nextButton.enabled = self.currentNodeIndex != (self.solution.count - 1);
 	self.previousButton.enabled = self.currentNodeIndex != 0;
 	self.pageLabel.text = [NSString stringWithFormat:@"%i of %i", self.currentNodeIndex + 1, self.solution.count];
 	
-	// update container 1
-	BSContainerState *containerState1 = node.states[self.containers[0].name];
-	self.containerNameLabel1.text = self.containers[0].name;
-	self.containerCapacityLabel1.text = [NSString stringWithFormat:@"%i", self.containers[0].capasity];
-	self.containerValueLabel1.text = [NSString stringWithFormat:@"%i", containerState1.value];
-	
-	// update container 2
-	BSContainerState *containerState2 = node.states[self.containers[1].name];
-	self.containerNameLabel2.text = self.containers[1].name;
-	self.containerCapacityLabel2.text = [NSString stringWithFormat:@"%i", self.containers[1].capasity];
-	self.containerValueLabel2.text = [NSString stringWithFormat:@"%i", containerState2.value];
-	
-	// update container 3
-	BSContainerState *containerState3 = node.states[self.containers[2].name];
-	self.containerNameLabel3.text = self.containers[2].name;
-	self.containerCapacityLabel3.text = [NSString stringWithFormat:@"%i", self.containers[2].capasity];
-	self.containerValueLabel3.text = [NSString stringWithFormat:@"%i", containerState3.value];
+	for (NSUInteger containerIndex = 0; containerIndex < self.containers.count; containerIndex++)
+	{
+		BSContainer *container = self.containers[containerIndex];
+		BSContainerState *containerState = node.states[container.name];
+		
+		self.containerValuesLabels[containerIndex].text = [NSString stringWithFormat:@"%i", containerState.value];
+		self.containerCapacityLabels[containerIndex].text = [NSString stringWithFormat:@"(%i)", container.capasity];
+	}
 }
 
 #pragma mark - Factory Methods
